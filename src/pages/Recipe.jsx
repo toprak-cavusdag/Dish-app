a40e369ff6cc59b5f393311fdab6c529a2fd26bc
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-03-09 12:38:54 +0300

    push

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 const Recipe = () => {
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState('instruction');
   let params = useParams();
   const apiKey = process.env.REACT_APP_API_KEY;
 
@@ -23,7 +24,28 @@ const Recipe = () => {
     FetchDetails();
   }, [params.name]);
 
-  return <div>{details.title}</div>;
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+      <Info>
+        <Button
+          className={activeTab === 'instruction' ? 'active' : ''}
+          onClick={() => setActiveTab('instruction')}
+        >
+          Instruction
+        </Button>
+        <Button
+          className={activeTab === 'ingredients' ? 'active' : ''}
+          onClick={() => setActiveTab('ingredients')}
+        >
+          Ingredients
+        </Button>
+      </Info>
+    </DetailWrapper>
+  );
 };
 
 const DetailWrapper = styled.div`
@@ -31,30 +53,35 @@ const DetailWrapper = styled.div`
   margin-bottom: 5rem;
   display: flex;
 
-  .active{
-    background: linear-gradient:(35deg, #494949, #313131)
-    color:white;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin-bottom: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
   }
 
-  h2{
-    margin-bottom:2rem;
+  ul {
+    margin-top: 2rem;
   }
-  li{
-    font-size:1.2rem;
-    line-height:2.5rem;
-  }
-
-  ul{
-    margin-top:2rem;
-  }
-
-
 `;
 
 const Button = styled.button`
   padding: 1rem 2rem;
   color: #313131;
   background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
 `;
 
 export default Recipe;
